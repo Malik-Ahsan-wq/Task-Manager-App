db2d6a2f9d60148cb89195773a9176a53587343b
Ahsan Malik <ahsanmalikking57@gmail.com>
2026-03-20 13:46:28 +0500

    bcrypt

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -12,7 +12,7 @@ router.post("/register", async (req, res) => {
   if (await User.findOne({ email }))
     return res.status(409).json({ message: "Email already in use" });
 
-  const hashed = await bcrypt.hash(password, 10);
+  const hashed = await bcrypt.hash(password, 8);
   const user = await User.create({ name, email, password: hashed });
 
   res.status(201).json({ message: "User registered successfully", userId: user._id });
